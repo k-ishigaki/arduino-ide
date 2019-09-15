ac4690901cd0f1094da5b005287c413005dcc7ac
Kazuki Ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2019-09-15 17:31:10 +0000

    Add batch file for writing hex

---
+++ write_hex.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+rem Prease set arduino program path
+set ARDUINO_HOME=C:\Program Files (x86)\Arduino
+set TARGET=atmega328p
+set HEX_FILE=.pio\build\uno\firmware.hex
+
+if "%1" == "" goto usage
+
+"%ARDUINO_HOME%\hardware\tools\avr\bin\avrdude" -C"%ARDUINO_HOME%\hardware/tools/avr/etc/avrdude.conf" -q -q -p"%TARGET%" -carduino -P%1 -b115200 -D -Uflash:w:"%HEX_FILE%":i
+
+goto :EOF
+
+:usage
+echo usage: %0 ^<port^>
+echo example: %0 COM0 .pio\build\uno\firmware.hex
