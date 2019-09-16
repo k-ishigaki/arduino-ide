d75679184b8dda9e8b569b28510f1aedfc4b1365
Kazuki Ishigaki <k-ishigaki@frontier.hokudai.ac.jp>
2019-09-16 02:13:28 +0000

    Modify control structure

--- write_hex.bat
+++ write_hex.bat
@@ -6,12 +6,10 @@ set ARDUINO_HOME=C:\Program Files (x86)\Arduino
 set TARGET=atmega328p
 set HEX_FILE=.pio\build\uno\firmware.hex
 
-if "%1" == "" goto usage
+if "%1" == "" (
+    echo usage: %0 ^<port^>
+    echo example: %0 COM0 .pio\build\uno\firmware.hex
+    exit /b 1
+)
 
 "%ARDUINO_HOME%\hardware\tools\avr\bin\avrdude" -C"%ARDUINO_HOME%\hardware/tools/avr/etc/avrdude.conf" -q -q -p"%TARGET%" -carduino -P%1 -b115200 -D -Uflash:w:"%HEX_FILE%":i
-
-goto :EOF
-
-:usage
-echo usage: %0 ^<port^>
-echo example: %0 COM0 .pio\build\uno\firmware.hex
